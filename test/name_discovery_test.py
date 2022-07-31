491b71c557385cba7f4cfb6631e5cc753142e2cf
Buck Baskin <mobile.wbaskin@gmail.com>
2022-07-30 18:44:52 -0700

    Name discovery test demonstrates source of NameError

--- test/name_discovery_test.py
+++ test/name_discovery_test.py
@@ -1,6 +1,6 @@
 from random_code import nested_unpack
 
-from ast import Module
+from ast import Module, Expr, IfExp
 
 
 def _strip_module(ast):
@@ -8,7 +8,13 @@ def _strip_module(ast):
         body = ast.body
         for expr in body:
             return expr
-    return ast
+    raise ValueError("No Module to strip")
+
+
+def _strip_expr(ast):
+    if isinstance(ast, Expr):
+        return ast.value
+    raise ValueError("No Expr to strip")
 
 
 def str_to_ast(s):
@@ -51,9 +57,38 @@ def test_BoolOp():
 
 # Call
 # ClassDef
+def test_ClassDef_simple():
+    ast = str_to_ast(
+        """
+class Major(name):
+    pass"""
+    )
+    assert ["name"] == nested_unpack(ast)
+
+
 # Compare
 # Constant
+def test_Constant():
+    ast = str_to_ast("""0""")
+    assert [] == nested_unpack(ast)
+
+
 # Delete
+def test_Delete_simple():
+    ast = str_to_ast("""del name""")
+    assert ["name"] == nested_unpack(ast)
+
+
+def test_Delete_subscript():
+    ast = str_to_ast("""del name[0]""")
+    assert ["name"] == nested_unpack(ast)
+
+
+def test_Delete_member():
+    ast = str_to_ast("""del name.other""")
+    assert ["name"] == nested_unpack(ast)
+
+
 # Dict
 # DictComp
 # dump
@@ -63,9 +98,34 @@ def test_BoolOp():
 # For
 # FormattedValue
 # FunctionDef
+def test_FunctionDef_typing():
+    ast = str_to_ast(
+        """
+def simple(x: name):
+    pass"""
+    )
+    assert ["name"] == nested_unpack(ast)
+
+
+def test_FunctionDef_decorator():
+    ast = str_to_ast(
+        """
+@name
+def simple(x):
+    pass"""
+    )
+    assert ["name"] == nested_unpack(ast)
+
+
 # GeneratorExp
 # If
 # IfExp
+def test_IfExp():
+    ast = _strip_expr(str_to_ast("""0 if name else 1"""))
+    assert isinstance(ast, IfExp)
+    assert ["name"] == nested_unpack(ast)
+
+
 # Import
 # ImportFrom
 # Index
@@ -87,5 +147,17 @@ def test_BoolOp():
 # Starred
 # Subscript
 # Try
+def test_Try():
+    ast = str_to_ast(
+        """
+try:
+    name()
+except:
+    pass
+"""
+    )
+    assert ["name"] == nested_unpack(ast)
+
+
 # Tuple
 # UnaryOp
